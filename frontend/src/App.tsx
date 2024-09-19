15cd17330985624535541cc956def92bb41fb755
ramuneri <153389006+ramuneri@users.noreply.github.com>
2024-09-19 15:44:06 +0300

    Frontend preparations (#8)
    * frontend preparation
    
    * Basic navbar added
    
    * Additions to navbar
    
    * Deleted unnecessary files
    
    ---------
    
    Co-authored-by: Leonardas <leonardas.sinkevicius@mif.stud.vu.lt>
    Co-authored-by: Aleksej Krasavcev <aleksej.krasavcev@mif.stud.vu.lt>
    Co-authored-by: Aleksej Krasavcev <144383208+HARCHBOB@users.noreply.github.com>
    Co-authored-by: Ramunė Riaubaitė <ramune.riaubaite@mif.stud.vu.lt>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
+import './App.css';
 import { Home } from './pages/Home';
+import { SignUp } from './pages/SignUp';
+import { LogIn } from './pages/LogIn';
+import Navbar from './components/navbar/Navbar';
 
-export const App = () =>  {
-
+export const App = () => {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path={'/'} element={<Home />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
       </Routes>
     </BrowserRouter>
-  )
-}
-
+  );
+};
---
+++ frontend/src/components/navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-left">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            LifeCouldBeDream
+          </Link>
+        </li>
+      </ul>
+      <ul className="nav-right">
+        <li className="nav-item">
+          <Link to="/signup" className="nav-link">
+            SIGN UP
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            LOGIN
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
