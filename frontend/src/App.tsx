2a1504846f534f0d554a02a04899a130552e0cdd
Ramunė Riaubaitė <ramune.riaubaite@mif.stud.vu.lt>
2024-09-19 09:50:48 +0300

    Deleted unnecessary files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Home } from './pages/Home';
-import { About } from './pages/About';
-import { Games } from './pages/Games';
 import { SignUp } from './pages/SignUp';
 import { LogIn } from './pages/LogIn';
 import Navbar from './components/navbar/Navbar';
@@ -13,8 +11,6 @@ export const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/games" element={<Games />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
       </Routes>
