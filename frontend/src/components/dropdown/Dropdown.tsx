7f66a75346322a86ff68d84fa8bdd6b53459b181
Leonardas Sinkevicius <141874292+LimeFizzy@users.noreply.github.com>
2024-11-11 15:39:01 +0200

    make dropdown beautiful (#34)

--- frontend/src/components/dropdown/Dropdown.tsx
+++ frontend/src/components/dropdown/Dropdown.tsx
@@ -12,33 +12,44 @@ enum GameTypes {
 }
 
 export const Dropdown = ({ onSelectChange }: DropdownProps) => {
-  const [selectedValue, setSelectedValue] = useState("");
+  const [selectedValue, setSelectedValue] = useState(
+    GameTypes.LONG_NUMBER as string
+  );
+  const [isOpen, setIsOpen] = useState(false);
   const options = [
     { value: GameTypes.LONG_NUMBER, label: "Long number memory" },
     { value: GameTypes.CHIMP, label: "Chimp test" },
     { value: GameTypes.SEQUENCE, label: "Sequence memory" },
   ];
 
-  const handleChange = useCallback(
-    (e: React.ChangeEvent<HTMLSelectElement>) => {
-      const value = e.target.value;
-      setSelectedValue(value);
-      onSelectChange(value);
-    },
-    []
-  );
+  const handleOptionClick = useCallback((value: string) => {
+    setSelectedValue(value);
+    onSelectChange(value);
+    setIsOpen(false);
+  }, []);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
 
   return (
-    <select
-      className="custom-select"
-      value={selectedValue}
-      onChange={handleChange}
-    >
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.label}
-        </option>
-      ))}
-    </select>
+    <div className="dropdown-container">
+      <div className="custom-select" onClick={toggleDropdown}>
+        {selectedValue
+          ? options.find((opt) => opt.value === selectedValue)?.label
+          : "Select an option"}
+      </div>
+      {isOpen && (
+        <div className="dropdown-options">
+          {options.map((option) => (
+            <div
+              key={option.value}
+              className="dropdown-option"
+              onClick={() => handleOptionClick(option.value)}
+            >
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
