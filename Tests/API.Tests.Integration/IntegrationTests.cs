2454c7ce51a9aeb1f0621c403ec7a841cd8ac27e
Aleksej Krasavcev <aleksej.krasavcev@mif.stud.vu.lt>
2024-11-25 23:46:06 +0200

    Integration test might be done

--- Tests/API.Tests.Integration/IntegrationTests.cs
+++ Tests/API.Tests.Integration/IntegrationTests.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.Testing;
@@ -7,23 +6,24 @@ using System.Text;
 
 namespace API.Tests.Integration
 {
-    public class BasicTests
+    public class IntegrationTests
         : IClassFixture<WebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
+        private const string username = "test10";
+        private const string password = "Test123.";
+        private const string level = "2";
 
-        public BasicTests(WebApplicationFactory<Program> factory)
+        public IntegrationTests(WebApplicationFactory<Program> factory)
         {
             _factory = factory;
         }
 
         [Theory]
-        [InlineData("/api/auth/register", "POST", "{\"Username\":\"test10\",\"Password\":\"Test123.\",\"IsAdmin\":true}")]
-        [InlineData("/api/auth/login", "POST", "{\"Username\":\"test10\",\"Password\":\"Test123.\",\"IsAdmin\":true}")]
-        [InlineData("/api/longnumber/generate-sequence/1", "GET", null)]
-        [InlineData("/api/sequence/generate-sequence/1", "GET", null)]
-        [InlineData("/api/userscore/leaderboard/longNumberMemory", "GET", null)]
-        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url, string method, string body)
+        [InlineData("/api/auth/register", "POST", $"{{\"Username\":\"{username}\",\"Password\":\"{password}\"}}")]
+        [InlineData("/api/auth/login", "POST", $"{{\"Username\":\"{username}\",\"Password\":\"{password}\"}}")]
+        [InlineData($"/api/auth/is-admin/{username}", "GET", null)]
+        public async Task Auth_EndpointsReturnSuccessAndCorrectContentType(string url, string method, string body)
         {
             // Arrange
             var client = _factory.CreateClient();
@@ -40,61 +40,64 @@ namespace API.Tests.Integration
                 response = await client.GetAsync(url);
             }
 
+            // Get and log the response body
+            var responseBody = await response.Content.ReadAsStringAsync();
+            
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.Equal("application/json; charset=utf-8",
-                response.Content.Headers.ContentType.ToString());
+
+            //Assert.False(true, responseBody);     // for testing purpose only!
+            Assert.Equal("application/json; charset=utf-8", response.Content.Headers.ContentType.ToString());
         }
 
         [Theory]
-        [InlineData("/api/UserScore/submit-score/longNumberMemory", "POST", "{\"username\":\"testUser\",\"gameType\":\"longNumberMemory\",\"guessedSequence\":[1,2,3],\"correctSequence\":[1,2,3],\"level\":1}")]
-        [InlineData("/api/UserScore/submit-score/sequenceMemory", "POST", "{\"username\":\"testUser\",\"gameType\":\"sequenceMemory\",\"guessedSequence\":[4,5,6],\"correctSequence\":[4,5,6],\"level\":2}")]
-        public async Task SubmitScore_EndpointsReturnSuccessAndCorrectContentType(string url, string method, string body)
+        [InlineData(
+            "/api/UserScore/submit-score/longNumberMemory", 
+            "/api/UserScore/leaderboard/longNumberMemory", 
+            $"{{\"username\":\"{username}\",\"gameType\":\"longNumberMemory\",\"level\":{level}}}"
+            )]
+        [InlineData(
+            "/api/UserScore/submit-score/sequenceMemory", 
+            "/api/UserScore/leaderboard/sequenceMemory", 
+            $"{{\"username\":\"{username}\",\"gameType\":\"sequenceMemory\",\"level\":{level}}}"
+            )]
+        public async Task SubmitScore_EndpointsReturnSuccessAndCorrectContentType(string submitUrl, string leaderboardUrl, string body)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var content = new StringContent(body ?? "{}", Encoding.UTF8, "application/json");
+            var submissionResponse = await client.PostAsync(submitUrl, content);
+            var leaderboardResponse = await client.GetAsync(leaderboardUrl);
+
+            // Assert
+            submissionResponse.EnsureSuccessStatusCode(); // Status Code 200-299
+            leaderboardResponse.EnsureSuccessStatusCode();
+            
+            Assert.Equal("application/json; charset=utf-8", submissionResponse.Content.Headers.ContentType.ToString());
+            Assert.Equal("application/json; charset=utf-8", leaderboardResponse.Content.Headers.ContentType.ToString());
+        }
+
+        [Theory]
+        [InlineData($"/api/longnumber/generate-sequence/{level}")]
+        [InlineData($"/api/sequence/generate-sequence/{level}")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
         {
             // Arrange
             var client = _factory.CreateClient();
             HttpResponseMessage response;
 
             // Act
-            if (method == "POST")
-            {
-                var content = new StringContent(body ?? "{}", Encoding.UTF8, "application/json");
-                response = await client.PostAsync(url, content);
-            }
-            else
-            {
-                throw new NotSupportedException($"HTTP method {method} is not supported in this test.");
-            }
-
-            // Assert
-            response.EnsureSuccessStatusCode(); // Status Code 200-299
-            Assert.Equal("application/json; charset=utf-8",
-                response.Content.Headers.ContentType.ToString());
-        }
-
-
-        [Theory]
-        [InlineData("/api/UserScore/leaderboard/longNumberMemory", "GET", null)]
-        [InlineData("/api/UserScore/leaderboard/sequenceMemory", "GET", null)]
-        public async Task Leaderboard_EndpointsReturnSuccessAndCorrectContentType(string url, string method, string body)
-        {
-            // Arrange
-            var client = _factory.CreateClient();
-            HttpResponseMessage response;
-
-            // Act
-            if (method == "POST")
-            {
-                var content = new StringContent(body ?? "{}", Encoding.UTF8, "application/json");
-                response = await client.PostAsync(url, content);
-            }
-            else
-            {
                 response = await client.GetAsync(url);
-            }
 
+            // Get and log the response body
+            var responseBody = await response.Content.ReadAsStringAsync();
+            
             // Assert
             response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            //Assert.False(true, responseBody);     // for testing purpose only!
             Assert.Equal("application/json; charset=utf-8",
                 response.Content.Headers.ContentType.ToString());
         }
